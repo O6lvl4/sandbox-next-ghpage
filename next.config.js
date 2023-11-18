91233465b79a0a3bce8f1faa8e83f1e2052f7102
O6lvl4 <motodera@phismize.com>
2023-11-18 09:29:47 +0900

    Add GitHubPages config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/sandbox-next-ghpage',
+    assetPrefix: '/sandbox-next-ghpage',
 }
 
 module.exports = nextConfig
+//o6lvl4.github.io/sandbox-next-ghpage
